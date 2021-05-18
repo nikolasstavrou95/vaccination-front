a538a3e7869ffbd186fde908a627c1422af4c088
Το Όνομά Σας <you@example.com>
2021-05-18 23:08:04 +0300

    delete background image

--- src/views/User.vue
+++ src/views/User.vue
@@ -1,11 +1,5 @@
 <template>
   <div>
-    <img
-      class="side_wave"
-      v-if="currentRoute != 'Statistics'"
-      src="/wave.png"
-    />
-
     <router-view></router-view>
   </div>
 </template>
@@ -25,12 +19,4 @@ export default {
 </script>
 
 <style>
-.side_wave {
-  position: fixed;
-  bottom: 0;
-  right: 0;
-  height: 100%;
-  z-index: -2;
-  transform: rotate(-90deg) translateX(-40%);
-}
 </style>
