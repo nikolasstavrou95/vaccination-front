ea67e1a684e002ad9064e2523200f9a5a2b8cee8
Vasiliki Georgakoudi <vasilikigeorgakoudi@gmail.com>
2021-05-24 13:58:51 +0300

    adding vuex-persistedstate plugin

--- src/store/index.js
+++ src/store/index.js
@@ -4,12 +4,18 @@ import Vuex from 'vuex';
 import { auth } from './auth.module';
 import * as patients from '@/store/modules/patients.js'
 import * as hospital from '@/store/modules/hospital.js'
+import createPersistedState from "vuex-persistedstate";
+
 Vue.use(Vuex);
 
+const dataState = createPersistedState({
+  paths: ['hospital']
+})
 export default new Vuex.Store({
   modules: {
     auth,
     patients,
     hospital
-  }
+  },
+  plugins: [dataState],
 });
