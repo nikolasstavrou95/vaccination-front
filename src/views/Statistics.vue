bc4c8148ad0001a0c5e726335f4285b6fa1aaa6f
Vasiliki Georgakoudi <vasilikigeorgakoudi@gmail.com>
2021-05-25 21:43:33 +0300

    implement statistics view with backend endpoind

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,40 +1,27 @@
 <template>
   <v-container fluid class="statistics">
-    <bar-chart :chart-data="citiesChartData" :options="options"></bar-chart>
+    <bar-chart v-if="citiesChartData.length>0" :chart-data="citiesChartData" :chartColors="colors" :options="options" label="Vaccinations per City"></bar-chart>
     <pie-chart class="chart pt-4"></pie-chart>
-    <bar-chart :chart-data="hospitalsChartData" :options="options"></bar-chart>
+     <bar-chart v-if="countriesChartData.length>0" :chart-data="countriesChartData" :chartColors="colors" :options="options" label="Vaccinations per Country"></bar-chart>
+    <bar-chart v-if="hospitalsChartData.length>0" :chart-data="hospitalsChartData" :chartColors="colors" :options="options" label="Vaccinations per Hospitals"></bar-chart>
+     <bar-chart v-if="agesChartData.length>0" :chart-data="agesChartData" :chartColors="colors" :options="options" label="Vaccinations per Age"></bar-chart>
   </v-container>
 </template>
 
 <script>
 import PieChart from "../components/charts/PieChart.vue";
 import BarChart from "../components/charts/BarChart.vue";
-
+import StatisticsDataService from "@/services/statistics.service.js";
 export default {
   components: { PieChart, BarChart },
   name: "Statistics",
 
   data() {
     return {
-      hospitalsChartData: {
-        labels: [
-          "Erythros Stavros",
-          "International Hospital",
-          "Ipokratio",
-          "Papanikolaou Hospital",
-          "Papageorgiou Hospital",
-          "Ahepa",
-          "Agios Loukas",
-          "Agios Therapodas",
-          "Tzanio",
-          "Alexadrio",
-          "International care",
-        ],
-        datasets: [
-          {
-            label: "Vaccination Per Hospital",
-            borderWidth: 1,
-            backgroundColor: [
+      data:[],
+      hospitalsChartData:[],
+      colors:{
+         backgroundColor: [
               "rgba(255, 99, 132, 0.2)",
               "rgba(54, 162, 235, 0.2)",
               "rgba(255, 206, 86, 0.2)",
@@ -48,7 +35,7 @@ export default {
               "rgba(153, 102, 255, 0.2)",
               "rgba(255, 159, 64, 0.2)",
             ],
-            borderColor: [
+         borderColor: [
               "rgba(255,99,132,1)",
               "rgba(54, 162, 235, 1)",
               "rgba(255, 206, 86, 1)",
@@ -62,58 +49,13 @@ export default {
               "rgba(153, 102, 255, 1)",
               "rgba(255, 159, 64, 1)",
             ],
-            pointBorderColor: "#2554FF",
-            data: [120, 190, 320, 50, 200, 300, 200, 320, 510, 620, 200, 100],
-          },
-        ],
+        pointBorderColor: "#2554FF",
       },
-      citiesChartData: {
-        labels: [
-          "Thessaloniki",
-          "Athens",
-          "Larisa",
-          "Volos",
-          "Chania",
-          "Heraclio",
-          "Alexadroupoli",
-        ],
-        datasets: [
-          {
-            label: "Vaccinations Per City",
-            borderWidth: 1,
-            backgroundColor: [
-              "rgba(255, 99, 132, 0.2)",
-              "rgba(54, 162, 235, 0.2)",
-              "rgba(255, 206, 86, 0.2)",
-              "rgba(75, 192, 192, 0.2)",
-              "rgba(153, 102, 255, 0.2)",
-              "rgba(255, 159, 64, 0.2)",
-              "rgba(255, 99, 132, 0.2)",
-              "rgba(54, 162, 235, 0.2)",
-              "rgba(255, 206, 86, 0.2)",
-              "rgba(75, 192, 192, 0.2)",
-              "rgba(153, 102, 255, 0.2)",
-              "rgba(255, 159, 64, 0.2)",
-            ],
-            borderColor: [
-              "rgba(255,99,132,1)",
-              "rgba(54, 162, 235, 1)",
-              "rgba(255, 206, 86, 1)",
-              "rgba(75, 192, 192, 1)",
-              "rgba(153, 102, 255, 1)",
-              "rgba(255, 159, 64, 1)",
-              "rgba(255,99,132,1)",
-              "rgba(54, 162, 235, 1)",
-              "rgba(255, 206, 86, 1)",
-              "rgba(75, 192, 192, 1)",
-              "rgba(153, 102, 255, 1)",
-              "rgba(255, 159, 64, 1)",
-            ],
-            pointBorderColor: "#2554FF",
-            data: [120, 190, 320, 50, 200, 300, 400],
-          },
-        ],
-      },
+      citiesChartData:[],
+      countriesChartData:[],
+      agesChartData:[],
+    
+      
 
       options: {
         scales: {
@@ -143,6 +85,43 @@ export default {
       },
     };
   },
+  methods:{
+     async getData(){
+     const response = await StatisticsDataService.getMetadata("PENDING");
+     console.log(response.data)
+     var tempdata = response.data
+    
+     this.createTables(tempdata,this.hospitalsChartData,"hospital-name")
+     this.createTables(tempdata,this.citiesChartData,"hospital-city")
+     this.createTables(tempdata,this.countriesChartData,"hospital-country");
+     this.createTables(tempdata,this.agesChartData,"age");
+    },
+    createTables(data,chart,groupby){
+      let collection = data.reduce((r, a) => {
+  
+        r[a.metadata[groupby]] = [...r[a.metadata[groupby]] || [], a];
+        return r;
+       }, {});
+       console.log("group", collection);
+        let objectArray = Object.entries(collection);
+      
+       objectArray.forEach(([key, value]) => {
+       chart.push({label: key, totals:value.length})
+        
+    });
+
+    }
+
+  },
+
+  
+
+
+    
+  
+  mounted(){
+    this.getData();
+  }
 };
 </script>
 <style  scoped>
