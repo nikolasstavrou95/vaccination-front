b2d4b5395d43ecc46934ceeb96e225e6d0d4fc75
Vasiliki Georgakoudi <vasilikigeorgakoudi@gmail.com>
2021-05-28 20:32:59 +0300

    fixing an error

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -95,7 +95,8 @@ export default {
      console.log(merged)
      //filter by status 
      var filtered =merged.filter((arr)=>
-       arr["1"]["vaccination-status"]!=="PENDING"
+       arr["1"]["vaccination-status"]==="PENDING" ||
+       arr["1"]["status"]=="PENDING"
 
      )
      console.log(filtered)
